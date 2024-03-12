533f734b2055ebc31f97448ba6084ef4e58c6040
nabizzs <162719159+nabizzs@users.noreply.github.com>
2024-03-12 11:24:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://www.rbsdirect.com.br/filestore/1/6/2/8/2_41a204c28718c76/28261_f8c8464d78d440c.jpg?w=700";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://www.lovz.com.br/wp-content/uploads/2017/06/relembre-tudo-sobre-namoro-justin-bieber-selena-gomez-02-LOVZ.jpg"
+}
+   
+
